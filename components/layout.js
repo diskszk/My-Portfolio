a85ea3e1b72e5a9a84bafd95d0306b1c06b559e6
diskszk <ibalucky23@gmail.com>
2020-08-20 11:27:17 +0900

    fix details

--- components/layout.js
+++ components/layout.js
@@ -2,38 +2,34 @@ import Head from 'next/head'
 import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
 export const siteTitle = `Daizuke Suzukiのポートフォリオサイト`;
 
 const Layout = ({ children, home }) => {
   return (
-    <div>
+    <>
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         {/* ファビコン,メタ情報等を書いていく */}
       </Head>
-
-      {/* Home画面ではheaderを非表示 */}
-      {!home && (
-      <header>
-        <div>
-          <Link href="/profile">
-            <a>Profile</a>
-          </Link>
+      <header className={styles.header}>
+        <div className={styles.navigation_nemu}>
+          <IconButton className={styles.humbergerMenu}>
+            <label>
+              <MenuIcon />
+            </label>
+          </IconButton>
         </div>
-        <div>
-          <Link href="/works">
-            <a>Works</a>
-          </Link>
-        </div>
-        <div>
+        <div className={styles.header_top}>
           <Link href="/">
-            <a>Home</a>
+            <a>My Portfolio</a>
           </Link>
         </div>
       </header>
-      )}
       <main>{children}</main>
-    </div>
+    </>
   );
 }
 
