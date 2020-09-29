cf688edfe18cbc4cc1c3b87bce593b0fcd6a964c
diskszk <ibalucky23@gmail.com>
2020-09-30 00:59:20 +0900

    add image

---
+++ components/common/ErrorMessage.tsx
@@ -0,0 +1,10 @@
+const ErrorMessage = () => {
+  return (
+    <div className="error-message">
+      <h1>通信エラーが発生しました。</h1>
+      <p>通信環境をご確認の上もう一度お試しください。</p>
+    </div>
+  );
+}
+
+export default ErrorMessage
